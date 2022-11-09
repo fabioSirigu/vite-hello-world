710e9e693d2b6e3dde515693f20a75c88a4c90d5
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-09 15:12:50 +0100

    add initial and final commit with bonus

---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<template>
+      <div class="jumbo">
+            <strong>This is a jumbotron!</strong>
+      </div>
+</template>
+
+<style scoped>
+.jumbo {
+      width: 600px;
+      height: 300px;
+      background-color: paleturquoise;
+      line-height: 300px;
+}
+</style>
